04ffc266f2322296f356bb42964982c007ee2c67
高冠祺 <guanqi.gao@bodentech.com>
2019-09-16 14:49:38 +0800

    player

--- lib/view/splash_page.dart
+++ lib/view/splash_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapleplayer/view/home_page.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashPage extends StatefulWidget{
   @override
@@ -7,14 +11,36 @@ class SplashPage extends StatefulWidget{
 }
 
 class _SplashPageState extends State<SplashPage> {
+
+  VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.asset("assets/video/splash.mp4")
+    // 在初始化完成后必须更新界面
+      ..initialize().then((_) {
+        setState(() {
+          // 视频为播放状态
+          _controller.play();
+          // 循环播放
+          _controller.setLooping(true);
+        });
+      });
     //startHome();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (Platform.isAndroid) {
+      // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+      SystemUiOverlayStyle systemUiOverlayStyle =
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    } else {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    }
+
     return Scaffold(
       body: Image.asset(
         "assets/images/launch_image.png",
@@ -32,5 +58,11 @@ class _SplashPageState extends State<SplashPage> {
         MaterialPageRoute(builder: (context) => HomePage()),
       );
     });
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
